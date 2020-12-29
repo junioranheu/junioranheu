e3a1ea8775d491504cadf5c1ff6ea12f7d8a170a
Potinha19@ <juninholorena@hotmail.com>
2020-12-29 13:57:03 -0300

    olá

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,62 @@ import { useState } from "react";
 
 function Home() {
     return (
-        <div class="container">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+        <section>
+            <title>Anheu</title>
+            
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+                integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+                crossOrigin="anonymous"
+            />
 
-            <h1 class="text-center mt-5">Olá mundo</h1>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="">Anheu</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <Contador />
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="">Início</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Teste 1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Teste 2</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Teste 3</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-            <div class="mt-5">
-                <div class="row">
-                    <div class="col-sm" style={{backgroundColor: "pink"}}>
-                        Coluna 1 de 3
-                    </div>
-                    <div class="col-sm" style={{backgroundColor: "blue"}}>
-                        Coluna 2 de 3
-                    </div>
-                    <div class="col-sm" style={{backgroundColor: "yellow"}}>
-                        Coluna 3 de 3
+            <div class="container">
+                <h1 class="text-center mt-5">Olá mundo</h1>
+
+                <Contador />
+
+                <div class="mt-5 text-center">
+                    <div class="row">
+                        <div class="col-sm" style={{ backgroundColor: "pink" }}>
+                            Coluna 1 de 3
+                        </div>
+                        <div class="col-sm" style={{ backgroundColor: "lightBlue" }}>
+                            Coluna 2 de 3
+                        </div>
+                        <div class="col-sm" style={{ backgroundColor: "yellow" }}>
+                            Coluna 3 de 3
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <h6 class="text-center mt-5">/npm run dev/</h6>
-        </div>
+                <h6 class="text-center mt-5">/npm run dev/</h6>
+            </div>
+        </section>
     );
 }
 
@@ -41,7 +74,7 @@ function Contador() {
                 <p class="font-weight-bold">{contador}</p>
             </div>
 
-            <button class="btn btn-outline-primary mt-1"
+            <button class="btn btn-outline-dark mt-1"
                 onClick={adicionarContador}>Adicionar</button>
         </div>
     );
