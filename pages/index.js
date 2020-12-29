5c5ef9a88f7f23380bbcb3f62c8949fbd92df3af
Potinha19@ <juninholorena@hotmail.com>
2020-12-29 11:26:21 -0300

    ola mundo

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Olá!</h1>
+            <Contador />
+
+            <br />
+            <span>/npm run dev/</span>
+        </div>
+    );
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    );
+}
+
+export default Home;
